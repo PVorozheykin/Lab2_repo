7137a497234603f5f9a0f82a58ad99c038fb6578
VPS <vps@fictionmail.com>
2023-09-09 21:00:21 +0700

    Добавлен и настроен логгер.

--- Lab2Pro/Lab2Pro/Program.cs
+++ Lab2Pro/Lab2Pro/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Lab2Pro
+﻿using Serilog;
+
+namespace Lab2Pro;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.Console()
+            .WriteTo.File("log/file.log")
+            .CreateLogger();
+
+        Log.Information("Application start!");
+
+        // Работа
+        // 
+
+        Log.Information("Application stop!");
+        Log.CloseAndFlush();
     }
 }
